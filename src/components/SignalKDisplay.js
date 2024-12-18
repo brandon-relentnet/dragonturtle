ca4bcf1e712a9c7a857988904b538eaa6b017525
Brandon Harris <brandon@relentnet.com>
2024-12-18 13:57:43 -0600

    Added a simple API pull, currently using a dog images API but will switch to SignalK soon.

---
+++ src/components/SignalKDisplay.js
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const sk_api = process.env.NEXT_PUBLIC_SK_API;
+
+function SignalKDisplay() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    console.log("SK_API: ", sk_api);
+
+    const generate = () => {
+        const fetchData = async () => {
+            if (!sk_api) {
+                setError("SK_API not set");
+                return;
+            }
+
+            try {
+                const response = await fetch(sk_api);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchData();
+    }
+
+    useEffect(() => {
+        generate();
+    }, []);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>Signal K Display</h1>
+            <button
+                onClick={generate}
+                className='p-4 bg-indigo-500 text-white rounded-lg shadow-lg hover:bg-indigo-600 hover:scale-105 active:scale-95 transition-transform duration-300'
+            >
+                New Image
+            </button>
+            <Image src={data.message}
+                alt="Signal K"
+                width={500}
+                height={500}
+            />
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
+}
+
+export default SignalKDisplay;
